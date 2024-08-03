156cdf50a53583ab90b107f9b3beff39a8aae627
Ngolap <ngolap2001@gmail.com>
2024-08-03 23:05:35 +0700

    first commit

---
+++ ASM/db/Students.js
@@ -0,0 +1,31 @@
+﻿[{
+        "username": "lapnv",
+        "password": "123456",
+        "fullname": "Ngô Văn Lập",
+        "email": "lapnvpd07470@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2001-04-04",
+        "schoolfee": "1500000",
+        "marks": "0"
+    },
+    {
+        "username": "tamdang",
+        "password": "iloveyou",
+        "fullname": "Đặng Thị Mỹ Tâm",
+        "email": "tamdtm@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2002-06-06",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "ngolap2001",
+        "password": "123456",
+        "fullname": "Phạm Thị Nở",
+        "email": "nopt@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    }
+]
---
+++ ASM/js/app/Students.js
@@ -0,0 +1,31 @@
+﻿[{
+        "username": "ngolap2001",
+        "password": "123456",
+        "fullname": "Ngô Văn Lập",
+        "email": "lapnvpd07470@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2001-04-04",
+        "schoolfee": "1500000",
+        "marks": "0"
+    },
+    {
+        "username": "pheonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "email": "pheonv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "le anh dao",
+        "password": "123123",
+        "fullname": "le anh dao",
+        "email": "dao@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000", 
+        "marks": "0"
+    }
+]
